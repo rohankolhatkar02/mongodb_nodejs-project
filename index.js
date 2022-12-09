0472a3c22bd3dd472d882b346f4febec4d51c5b0
Rohan Kolhatkar <97934251+rohankolhatkar02@users.noreply.github.com>
2022-12-09 12:40:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,16 @@ var bodyParser = require("body-parser")
 var mongoose = require("mongoose")
 
 const app = express()
+const MongoClient = require('mongodb').MongoClient;
+const objectID = require('mongodb').ObjectId
+const url= 'mongodb://localhost:27017';
+
+const dbName = 'adbms';
+
+const client = new MongoClient(url);
+const assert = require('assert');
+const { callbackify } = require('util');
+const { Router } = require("express");
 
 app.use(bodyParser.json())
 app.use(express.json())
@@ -13,6 +23,8 @@ app.use(express.urlencoded({
 app.use(bodyParser.urlencoded({
     extended:true
 }))
+
+//mongodb connection
 
 mongoose.connect('mongodb://Localhost:27017/adbms',{
     useNewUrlParser: true,
@@ -25,7 +37,11 @@ db.on('error',()=>console.log("Error in Connecting to Database"))
 db.once('open',()=>console.log("Connected to Database"))
 
 
-// for data insertion
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs')
+
+// for data insertion in checkin page
 
 
 app.post("/sign_up",(req,res)=>{
@@ -48,14 +64,98 @@ app.post("/sign_up",(req,res)=>{
 })
  
 
-// for new collection
+// for menu ordering
+
+app.post("/menu",(req,res)=>{
+    //var volvo = req.body.volvo
+    var tableno = req.body.tableno;
+    var starters = req.body.starters;
+    var ramen = req.body.ramen;
+    var soft = req.body.soft;
+    var alcool = req.body.alcool;
+  
+  
+    var data = {
+         "tableno": tableno,
+         "starters":starters,
+         "ramen":ramen,
+         "soft": soft,
+         "alcool": alcool
+    }
+  
+  db.collection('order').insertOne(data,(err,collection)=>{
+    if(err){
+        throw err;
+    }
+    console.log("Record inserted successfully");
+  });
+  
+  return res.redirect('order.html')
+  })
+
+/*Update function*/
+
+app.post("/update",function(req,res, next){
+    var item = {
+         starters:req.body.starters,
+         ramen:req.body.ramen,
+         soft: req.body.soft,
+         alcool: req.body.alcool
+    };
+    
+    var tableno = req.body.tableno;
+
+    MongoClient.connect(url, function(err,client){
+        assert.equal(null,err);
+        db.collection('order').updateOne({"tableno":tableno}, {$set: item}, function(err, result){
+            assert.equal(null, err);
+            console.log('Record updated');
+            client.close();
+        });
+    });
+    return res.redirect('order.html')
+});
+
+/*Delete Function*/
+
+app.post("/delete", function(req, res, next){
+    var tableno = req.body.tableno;
+
+    MongoClient.connect(url, function(err,client){
+        assert.equal(null,err);
+        db.collection('order').deleteOne({"tableno":tableno}, function(err, result){
+            assert.equal(null, err);
+            console.log('Record deleted');
+            client.close();
+        });
+    });
+    return res.redirect('order.html')
+
+})
+
+
+//for Kitchen Display page
+
+
+
+app.get('/devices',(req,res) =>{
+    // let device_list = [{'name':'dht22'},{'name':'temp36'}]
+     const db = client.db(dbName);
+     const collection = db.collection('order');
+     collection.find({}).toArray(function(err,user_list){
+         assert.equal(err,null);
+         res.render('users',{'users': user_list})
+     });
+ })
+
+// for Connection to LocalHost
 
 
 app.get("/",(req,res)=>{
     res.set({
         "Allow-access-Allow-Origin":'*'
     })
-    return res.redirect('index.html')
+    return res.redirect('loader.html')
 }).listen(4000);
 
 console.log("listening on PORT 4000");
